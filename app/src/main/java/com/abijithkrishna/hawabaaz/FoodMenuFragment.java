9a804ea945de54fc77066a3d22f4ae729e50d863
Abijith Krishna <abijitharc@gmail.com>
2015-09-06 11:50:16 +0530

    Initial commit

---
+++ app/src/main/java/com/abijithkrishna/hawabaaz/FoodMenuFragment.java
@@ -0,0 +1,227 @@
+package balakumaran.hawabazz_app;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FoodMenuFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FoodMenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+
+
+public class FoodMenuFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 4 ;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FoodMenuFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FoodMenuFragment newInstance(String param1, String param2) {
+        FoodMenuFragment fragment = new FoodMenuFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public FoodMenuFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View x = inflater.inflate(R.layout.fragment_food_menu, container, false);
+        tabLayout = (TabLayout) x.findViewById(R.id.tabs);
+        viewPager = (ViewPager) x.findViewById(R.id.viewpager);
+
+        /**
+         *Set an Apater for the View Pager
+         */
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+
+        /**
+         * Now , this is a workaround ,
+         * The setupWithViewPager dose't works without the runnable .
+         * Maybe a Support Library Bug .
+         */
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+                //tabLayout.setOnTabSelectedListener(  );
+            }
+        });
+
+        return x;
+    }
+
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+    class MyAdapter extends FragmentPagerAdapter implements ViewPager.OnPageChangeListener,ActionBar.TabListener {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return fragment with respect to Position .
+         */
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 : return new JuiceFragment();
+                case 1 : return new SandwichFragment();
+                case 2 : return new MainCourseFragment();
+                case 3 : return new SpecialFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+
+            return int_items;
+
+        }
+
+        /**
+         * This method returns the title of the tab according to the position.
+         */
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0 :
+                    return "Juice";
+                case 1 :
+                    return "Sand wich";
+                case 2 :
+                    return "Main Course";
+                case 3 :
+                    return "Specials";
+            }
+            return null;
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/abijithkrishna/hawabaaz/MainActivity.java
@@ -0,0 +1,214 @@
+package balakumaran.hawabazz_app;
+
+import android.app.Activity;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Stack;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks,HomeFragment.OnFragmentInteractionListener,LocationFragment.OnFragmentInteractionListener,FoodMenuFragment.OnFragmentInteractionListener,JuiceFragment.OnFragmentInteractionListener,SandwichFragment.OnFragmentInteractionListener,MainCourseFragment.OnFragmentInteractionListener,SpecialFragment.OnFragmentInteractionListener,OrderHistoryFragment.OnFragmentInteractionListener,NotificationsFragment.OnFragmentInteractionListener,MyLocationsFragment.OnFragmentInteractionListener,RegisterFragment.OnFragmentInteractionListener,LoginFragment.OnFragmentInteractionListener,TrackOrderFragment.OnFragmentInteractionListener,CancelOrderFragment.OnFragmentInteractionListener {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+    private Stack<CharSequence> titileStack = new Stack<CharSequence>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        Fragment mCurr = null;
+        switch (position){
+            case 0:
+                mCurr = new HomeFragment();
+                break;
+            case 1:
+                mCurr = new LocationFragment();
+                break;
+            case 2:
+                mCurr = new FoodMenuFragment();
+                break;
+            case 3:
+                mCurr = new OrderHistoryFragment();
+                break;
+            case 4:
+                mCurr = new NotificationsFragment();
+                break;
+            case 5:
+                mCurr = new MyLocationsFragment();
+                break;
+            case 6:
+                mCurr = new RegisterFragment();
+                break;
+            case 7:
+                mCurr = new LoginFragment();
+                break;
+        }
+        onSectionAttached(position);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().addToBackStack(null)
+                .replace(R.id.container, mCurr)
+                .commit();
+        //mTitle = null;
+        //onSectionAttached(position);
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 0:
+                mTitle = "Home";
+                break;
+            case 1:
+                mTitle = "Location";
+                break;
+            case 2:
+                mTitle = "Food Menu";
+                break;
+            case 3:
+                mTitle = "Order History";
+                break;
+            case 4:
+                mTitle = "Notifications";
+                break;
+            case 5:
+                mTitle = "My Locations";
+                break;
+            case 6:
+                mTitle = "Register";
+                break;
+            case 7:
+                mTitle = "Login";
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        titileStack.push(mTitle);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.trackorder:
+            {
+                android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                TrackOrderFragment fragment = new TrackOrderFragment();
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                mTitle="Track Order";
+                //titileStack.push(mTitle);
+                restoreActionBar();
+                fragmentTransaction.commit();
+                return true;
+            }
+            case R.id.cancelorder:
+            {
+                android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                CancelOrderFragment fragment = new CancelOrderFragment();
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                mTitle="Cancel Order";
+                //titileStack.push(mTitle);
+                restoreActionBar();
+                fragmentTransaction.commit();
+                return true;
+            }
+            case R.id.callcare:{
+                Toast.makeText(getApplicationContext(), "YET TO COME",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        //return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //restoreActionBar();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+
+        try{
+            titileStack.pop();
+            actionBar.setTitle(titileStack.peek());
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        //actionBar.setTitle("Hawabazz");
+        super.onBackPressed();
+    }
+}
